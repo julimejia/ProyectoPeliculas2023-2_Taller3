20a541ffe72e0ca4b48770d388bfdaef8482f19e
julimejia <davidmejiao43@gmail.com>
2023-10-26 10:54:21 -0500

    ip externa

--- DjangoProjectBase/recomendations/views.py
+++ DjangoProjectBase/recomendations/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . import rec  # Asegúrate de que esta importación esté configurada correctamente
+ # Asegúrate de que esta importación esté configurada correctamente
 
 def recomendacion(request):
     if request.method == 'POST':
         search_movie = request.POST.get('searchMovie', '')  # Obtiene el valor del campo 'searchMovie' del formulario
 
         # Llama a la función rec con los datos del formulario
-        resultado = rec.recomendar(search_movie)
+        #resultado = rec.recomendar(search_movie)
 
         # Puedes hacer algo con el resultado, como mostrarlo en la plantilla o procesarlo de alguna otra manera
         return render(request, 'resultado.html', {'resultado': resultado})
